1c9bc62d4aa7a506593e01053bbaf92ec988bcc8
gawadesuraj <csemr.surajgawade@gmail.com>
2025-07-29 11:38:30 +0530

    Ejs Server side redering

--- SHORT-URL/index.js
+++ SHORT-URL/index.js
@@ -1,23 +1,33 @@
 const express = require("express");
 const { connectDB } = require("./connect");
-const router = require("./routers/urlRouter");
+const urlRouter = require("./routers/urlRouter");
 const URL = require("./models/url.model");
-const path = require('path');
+const path = require("path");
 const app = express();
 const PORT = 5000;
 
+const staticRouter = require("./routers/staticRouter");
+
 const url = "mongodb://127.0.0.1:27017/short-url";
 connectDB(url);
-app.use(express.json());
-app.use("/", router);
+
+
 app.set("view engine", "ejs");
-app.set("views", path.resolve("./views"))
-app.get('/test', async (req, res) =>{
-  const allUrls = await URL.find({})
-  return res.render('home',{
-    urls:allUrls
-  })
-})
+app.set("views", path.resolve("./views"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/url", urlRouter);
+app.use("/", staticRouter);
+
+app.get("/test", async (req, res) => {
+  const allUrls = await URL.find({});
+  return res.render("home", {
+    urls: allUrls,
+  });
+});
+
 app.get("/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
@@ -34,6 +44,7 @@ app.get("/:shortId", async (req, res) => {
   );
   res.redirect(entry.redirectURL);
 });
+
 app.listen(PORT, () => {
   console.log("Server is start at PORT : ", PORT);
 });
--- SHORT-URL/routers/urlRouter.js
+++ SHORT-URL/routers/urlRouter.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { handleGenerateNewShortURL, handleGetAnalytics } = require("../controllers/urlController");
 const router = express.Router();
 
-router.post('/url', handleGenerateNewShortURL)
+router.post('/', handleGenerateNewShortURL)
+
 router.get('/analytics/:shortId', handleGetAnalytics)
 
 module.exports = router
